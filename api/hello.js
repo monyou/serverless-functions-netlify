9cdadf8d57073259123459b5cfd67da3c59bee6e
Симеон Мечков <montishak@gmail.com>
2022-02-28 16:58:17 +0200

    ok

--- api/hello.js
+++ api/hello.js
@@ -1,26 +1,10 @@
-const nodemailer = require("nodemailer");
-
-const transporter = nodemailer.createTransport({
-  service: process.env.EMAIL_SERVICE_PROVIDER,
-  auth: {
-    user: process.env.EMAIL_SERVICE_AUTH_EMAIL,
-    pass: process.env.EMAIL_SERVICE_AUTH_PASS,
-  },
-});
-
 const handler = async (event, context) => {
-  await new Promise((resolve) => {
-    transporter.sendMail({ to: "monyou@abv.bg", text: "Hello World!" }, () =>
-      resolve()
-    );
-  });
-
   return {
     statusCode: 200,
     body: JSON.stringify({
-      message: "Hello World! Message sent successfully!",
+      message: "Hello World!",
     }),
   };
 };
 
-module.exports = { handler };
+export default { handler };
