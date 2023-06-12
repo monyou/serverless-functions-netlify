51a3b2d01b8621b060fbf259b7a87e663a525531
Simeon Mechkov <montishak@gmail.com>
2023-06-12 14:33:58 +0300

    new endpoint for vsg bob employees

---
+++ api/get_all_employees_data_from_bob.js
@@ -0,0 +1,52 @@
+const handler = async (event, context) => {
+  if (event.httpMethod === "GET") {
+    if (event.headers["x-token"] !== process.env.BOB_PUBLIC_KEY)
+      return {
+        statusCode: 403,
+        body: JSON.stringify({
+          message: "Not authorized",
+        }),
+      };
+
+    try {
+      const response = await fetch("https://api.hibob.com/v1/people", {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: process.env.BOB_API_KEY,
+        },
+      });
+      const data = await response.json();
+
+      const employees = data.employees.map((employee) => ({
+        name: employee.displayName,
+        avatar: employee.avatarUrl,
+        email: employee.email,
+      }));
+
+      return {
+        statusCode: 200,
+        body: JSON.stringify({
+          employees,
+        }),
+      };
+    } catch (error) {
+      return {
+        statusCode: 500,
+        body: JSON.stringify({
+          message: "Internal Server Error",
+          error,
+        }),
+      };
+    }
+  } else {
+    return {
+      statusCode: 405,
+      body: JSON.stringify({
+        message: "Method not allowed",
+      }),
+    };
+  }
+};
+
+module.exports = { handler };
--- api/vsg_parking_pal_login.js
+++ api/vsg_parking_pal_login.js
@@ -1,35 +1,52 @@
-const db = require("./_utils/dbConnection");
-const authorize = require("./_utils/authorize");
+const mysql = require("mysql");
+
+const db = mysql.createConnection({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.DB_NAME,
+});
 
 const handler = async (event, context) => {
   if (event.httpMethod === "POST") {
     try {
       const body = JSON.parse(event.body || "{}");
 
-      if (!(await authorize(db, body)).catch())
-        return {
-          statusCode: 401,
-          body: JSON.stringify({
-            message: "Unauthorized",
-          }),
-        };
-
-      const result = {
-        statusCode: 200,
-        message: "OK",
-      };
-
-      await new Promise((resolve, reject) => {
+      const result = await new Promise((resolve) => {
         db.query(
-          `SELECT token FROM vsg_pp_user WHERE email like '${body.email}'`,
+          `SELECT id, token FROM vsg_pp_user WHERE email like '${body.email}' AND password like '${body.password}'`,
           (error, results) => {
-            if (error) return reject(error);
+            if (error) return resolve(error);
 
-            result.body = JSON.stringify({ token: results[0].token });
-            resolve();
+            let response = {
+              id: "query",
+            };
+
+            if (results.length) {
+              response = {
+                ...response,
+                statusCode: 200,
+                body: JSON.stringify({
+                  id: results[0].id,
+                  token: results[0].token,
+                }),
+              };
+            } else {
+              response = {
+                ...response,
+                statusCode: 400,
+                body: JSON.stringify({
+                  message: "No user found with the given credentials",
+                }),
+              };
+            }
+
+            resolve(response);
           }
         );
-      }).catch();
+      });
+
+      if (!result.id) throw result;
 
       return result;
     } catch (error) {
@@ -37,6 +54,7 @@ const handler = async (event, context) => {
         statusCode: 500,
         body: JSON.stringify({
           message: "Internal Server Error",
+          error,
         }),
       };
     }
